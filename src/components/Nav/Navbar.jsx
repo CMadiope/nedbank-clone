a5f4d4341d2705d90d0675c86477497ca792f905
Confidence Madiope <confidencem97@gmail.com>
2023-05-04 17:07:31 +0200

    added onclick menu

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -3,23 +3,32 @@ import Link from "next/link";
 import React, { useState } from "react";
 import { AiOutlineLock, AiFillCaretDown } from "react-icons/ai";
 import { BiMenuAltRight } from "react-icons/bi";
+import { RxCross1 } from "react-icons/rx";
+import OnClickMenu from "./OnClickMenu";
 
 const Navbar = () => {
   const [showModal, setShowModal] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
   const handleModal = () => {
     setShowModal(!showModal);
   };
 
+  const handleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
-    <nav className='md:bg-[#f2f9f5] bg-white '>
+    <nav className={` ${showMenu ? "bg-green-800" : "md:bg-[#f2f9f5]"} `}>
       <div className='flex items-center justify-between pl-6 md:pl-20'>
-        <div className='flex gap-6 items-center'>
+        <div className={`flex gap-6 items-center `}>
           <Link href='/'>
             <Image src='/images/logo.svg' alt='logo' width={40} height={30} />
           </Link>
           <div
-            className='md:hidden flex items-center gap-1 cursor-pointer relative'
+            className={`md:hidden flex items-center gap-1 cursor-pointer relative ${
+              showMenu ? "hidden" : "flex"
+            }`}
             onClick={handleModal}
           >
             <h3 className=' uppercase font-bold'>Personal</h3>
@@ -49,34 +58,44 @@ const Navbar = () => {
             </div>
           )}
 
-          <ul className='md:flex gap-6 hidden font-semibold'>
-            <li>
-              <Link href='/personal'>Personal</Link>
-            </li>
-            <li>
-              <Link href='/private'>Private</Link>
-            </li>
-            <li>
-              <Link href='/wealth'>Wealth</Link>
-            </li>
-            <li>
-              <Link href='/business'>Business</Link>
-            </li>
-            <li>
-              <Link href='/corporate'>corporate</Link>
-            </li>
-          </ul>
+          <div className={`${showMenu ? "hidden" : "block"}`}>
+            <ul className='md:flex gap-6 hidden font-semibold'>
+              <li>
+                <Link href='/personal'>Personal</Link>
+              </li>
+              <li>
+                <Link href='/private'>Private</Link>
+              </li>
+              <li>
+                <Link href='/wealth'>Wealth</Link>
+              </li>
+              <li>
+                <Link href='/business'>Business</Link>
+              </li>
+              <li>
+                <Link href='/corporate'>corporate</Link>
+              </li>
+            </ul>
+          </div>
         </div>
         <div className='flex items-center gap-5'>
-          <div className='text-green-800 cursor-pointer flex items-center gap-3'>
+          <div
+            className={`text-green-800 cursor-pointer flex items-center gap-3 ${
+              showMenu ? "hidden" : "flex"
+            }`}
+          >
             <span className='text-black hidden md:block text-md'>Log in</span>
             <AiOutlineLock size={30} />
           </div>
-          <div className='p-3 bg-green-800 text-white cursor-pointer'>
-            <BiMenuAltRight size={40} />
+          <div
+            className='p-3 bg-green-800 text-white cursor-pointer'
+            onClick={handleMenu}
+          >
+            {showMenu ? <RxCross1 size={40} /> : <BiMenuAltRight size={40} />}
           </div>
         </div>
       </div>
+      {showMenu && <OnClickMenu />}
     </nav>
   );
 };
---
+++ src/components/Nav/OnClickMenu.jsx
@@ -0,0 +1,199 @@
+import React from "react";
+import {
+  AiOutlineRight,
+  AiOutlineLock,
+  AiOutlineArrowRight,
+} from "react-icons/ai";
+import { BsSearch } from "react-icons/bs";
+import { BiPhoneCall } from "react-icons/bi";
+import { CiLocationOn } from "react-icons/ci";
+import { motion } from "framer-motion";
+
+const OnClickMenu = () => {
+
+  const containerVariants = {
+    initial: {
+      opacity:0,
+      x:'-100vw'
+    },
+    animation:{
+      opacity:1,
+      x:0,
+      transition:{
+        delay:1.1,
+        duration:1.1
+      }
+    },
+    exist:{
+      x:"-100vw",
+      transition:{
+        ease:'easeInOut'
+      }
+    }
+  }
+
+  return (
+    <div className='bg-green-800 text-white/90 transition duration-300'>
+      {/* tablet and below */}
+      <div className='flex flex-col gap-3  py-10 md:hidden'>
+        <div className='flex items-center justify-between w-full border-t py-3 px-4 text-lg cursor-pointer'>
+          <h3>Bank</h3>
+          <AiOutlineRight />
+        </div>
+        <div className='flex items-center justify-between w-full border-t py-3 px-4 text-lg cursor-pointer'>
+          <h3>Save & Invest</h3>
+          <AiOutlineRight />
+        </div>
+        <div className='flex items-center justify-between w-full border-t py-3 px-4 text-lg cursor-pointer'>
+          <h3>Borrow</h3>
+          <AiOutlineRight />
+        </div>
+        <div className='flex items-center justify-between w-full border-t py-3 px-4 text-lg cursor-pointer'>
+          <h3>Insure</h3>
+          <AiOutlineRight />
+        </div>
+        <div className='flex items-center justify-between w-full border-t py-3 px-4 text-lg cursor-pointer'>
+          <h3>Plan</h3>
+          <AiOutlineRight />
+        </div>
+        <div className='flex items-center justify-between w-full border-t py-3 px-4 text-lg cursor-pointer'>
+          <h3>Learn</h3>
+          <AiOutlineRight />
+        </div>
+        <div className='flex items-center justify-between w-full border-y py-3 px-4 text-lg cursor-pointer'>
+          <h3>Contact us</h3>
+          <AiOutlineRight />
+        </div>
+        <div className='flex flex-col gap-6 text-lg py-8 cursor-pointer'>
+          <div className='flex gap-2 items-center pl-4'>
+            <BsSearch size={25} />
+            <p>Search</p>
+          </div>
+          <div className='flex pl-4 gap-2 items-center cursor-pointer'>
+            <AiOutlineLock size={25} />
+            <p>Log In</p>
+          </div>
+        </div>
+      </div>
+      {/* tablet and above */}
+      <motion.div className='max-md:hidden grid grid-cols-3 md:h-screen md:w-full py-16 px-12 gap-12'
+      initial='initial'
+      animate='animation'
+      variants={containerVariants}
+      exit='exit'
+      >
+        <div className='flex flex-col gap-6 font-bold text-3xl  text-right'>
+          <h1 className='hover:text-white/80 cursor-pointer transition'>
+            Bank
+          </h1>
+          <h1 className='hover:text-white text-white/70 cursor-pointer transition'>
+            Save & Invest
+          </h1>
+          <h1 className='hover:text-white text-white/70 cursor-pointer transition'>
+            Borrow
+          </h1>
+          <h1 className='hover:text-white text-white/70 cursor-pointer transition'>
+            Insure
+          </h1>
+          <h1 className='hover:text-white text-white/70 cursor-pointer transition'>
+            Plan
+          </h1>
+          <h1 className='hover:text-white text-white/70 cursor-pointer transition'>
+            Learn
+          </h1>
+          <h1 className='hover:text-white text-white/70 cursor-pointer transition'>
+            Contact us
+          </h1>
+          <div className='border-t'>
+            <div className='text-sm flex justify-end gap-3 pt-6 cursor-pointer'>
+              <BiPhoneCall size={20} />
+              <p>Get in touch</p>
+            </div>
+            <div className='text-sm flex justify-end gap-3 cursor-pointer py-2 '>
+              <CiLocationOn size={20} />
+              <p>Find us</p>
+            </div>
+          </div>
+        </div>
+        <div className='col-span-2'>
+          <h1 className='font-light text-3xl'>Manage your money</h1>
+          <div className='grid grid-cols-2 lg:grid-cols-3 p-8 gap-4'>
+            <div className='group  hover:bg-white/10 py-6 px-10 hover:shadow-xl hover:rounded-xl duration-50'>
+              <div className='flex justify-between group-hover:scale-110'>
+                <h4 className='font-bold pb-6'>Accounts</h4>
+                <div className='font-bold hidden group-hover:block '>
+                  <AiOutlineArrowRight size={20} />
+                </div>
+              </div>
+              <p className='text-white/60 group-hover:scale-110'>
+                Bank how and when you chose to , with the low fees or extra
+                benefits you want.
+              </p>
+            </div>
+            <div className='group  hover:bg-white/10 py-6 px-10 hover:shadow-xl hover:rounded-xl duration-50'>
+              <div className='flex justify-between group-hover:scale-110'>
+                <h4 className='font-bold pb-6'>Credit cards</h4>
+                <div className='font-bold hidden group-hover:block '>
+                  <AiOutlineArrowRight size={20} />
+                </div>
+              </div>
+              <p className='text-white/60 group-hover:scale-110'>
+                Shop online or instore and earn rewards for it, with up to 55
+                days interest-free credit.
+              </p>
+            </div>
+            <div className='group  hover:bg-white/10 py-6 px-10 hover:shadow-xl hover:rounded-xl duration-50'>
+              <div className='flex justify-between group-hover:scale-110'>
+                <h4 className='font-bold pb-6'>Digital banking</h4>
+                <div className='font-bold hidden group-hover:block '>
+                  <AiOutlineArrowRight size={20} />
+                </div>
+              </div>
+              <p className='text-white/60 group-hover:scale-110'>
+                Enjoy anytime-anywhere banking from your computer, cellphone or
+                tablet.
+              </p>
+            </div>
+            <div className='group  hover:bg-white/10 py-6 px-10 hover:shadow-xl hover:rounded-xl duration-50'>
+              <div className='flex justify-between group-hover:scale-110'>
+                <h4 className='font-bold pb-6'>Fraud awareness</h4>
+                <div className='font-bold hidden group-hover:block '>
+                  <AiOutlineArrowRight size={20} />
+                </div>
+              </div>
+              <p className='text-white/60 group-hover:scale-110'>
+                Protect yourself against the latest schemes and scams.
+              </p>
+            </div>
+            <div className='group  hover:bg-white/10 py-6 px-10 hover:shadow-xl hover:rounded-xl duration-50'>
+              <div className='flex justify-between group-hover:scale-110'>
+                <h4 className='font-bold pb-6'>Forex</h4>
+                <div className='font-bold hidden group-hover:block '>
+                  <AiOutlineArrowRight size={20} />
+                </div>
+              </div>
+              <p className='text-white/60 group-hover:scale-110'>
+                Buy foreign banknotes, transfer funds or shop globally with a
+                travel card.
+              </p>
+            </div>
+            <div className='group  hover:bg-white/10 py-6 px-10 hover:shadow-xl hover:rounded-xl duration-50'>
+              <div className='flex justify-between group-hover:scale-110'>
+                <h4 className='font-bold pb-6'>Rewards</h4>
+                <div className='font-bold hidden group-hover:block '>
+                  <AiOutlineArrowRight size={20} />
+                </div>
+              </div>
+              <p className='text-white/60 group-hover:scale-110'>
+                Manage your money like a pro and get rewarded. Epic deals,
+                discounts and more.
+              </p>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default OnClickMenu;
